9274b361b4b228069b1baca6ba1a7e034d37ac21
Alex Sammons <alsamon@yahoo.com>
2016-03-11 10:07:29 -0500

    Started Work on commenting and testing

---
+++ Frunction/src/net/samongi/frunction/parse/Parsing.java
@@ -0,0 +1,9 @@
+package net.samongi.frunction.parse;
+
+public class Parsing
+{
+	public String parseSplit(String text, String regex)
+	{
+		return null;
+	}
+}
---
+++ Frunction/test/net/samongi/frunction/parse/CommentingTest.java
@@ -0,0 +1,16 @@
+package net.samongi.frunction.parse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommentingTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
