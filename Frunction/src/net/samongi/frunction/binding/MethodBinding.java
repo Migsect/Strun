ae966cfae8504f1c20647684c49d7f0f1196be58
Alex Sammons <alsamon@yahoo.com>
2016-03-16 18:45:47 -0400

    Did some minor things

--- Frunction/src/net/samongi/frunction/binding/MethodBinding.java
+++ Frunction/src/net/samongi/frunction/binding/MethodBinding.java
@@ -1,7 +1,5 @@
 package net.samongi.frunction.binding;
 
-import java.util.List;
-
 import net.samongi.frunction.expression.Expression;
 import net.samongi.frunction.frunction.Frunction;
 
@@ -14,17 +12,24 @@ public class MethodBinding implements Binding
 {
 	private final Frunction container;
 	private final String[] input_symbols;
+	private final String[][] input_types;
+	
+	
 	private final String source;
 	
+	private Expression condition = null;
 	private Expression expression = null;
 
-	public MethodBinding(Frunction container, String[] input_symbols, String source)
+	public MethodBinding(Frunction container, String[] input_symbols, String[][] input_types, String cond_source, String source)
 	{
 		this.container = container;
 		this.input_symbols = input_symbols;
+		this.input_types = input_types;
 		this.source = source;
 	}
-
+	
+	
+	
 	@Override public String getSource(){return this.source;}
 
 	@Override public Frunction getContainer(){return this.container;}
