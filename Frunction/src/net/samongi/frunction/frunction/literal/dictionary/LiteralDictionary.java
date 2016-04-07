b7d3cdf3d8d0de23a95cdd1b7842b06545285fe8
Migsect <alsamon@yahoo.com>
2016-04-06 22:57:01 -0400

    missed some reformatting

--- Frunction/src/net/samongi/frunction/frunction/literal/dictionary/LiteralDictionary.java
+++ Frunction/src/net/samongi/frunction/frunction/literal/dictionary/LiteralDictionary.java
@@ -12,25 +12,25 @@ import net.samongi.frunction.frunction.literal.BooleanFrunction;
 import net.samongi.frunction.frunction.literal.NativeFrunction;
 import net.samongi.frunction.frunction.literal.StringFrunction;
 
-/**Represents a container that stores and creates all instances of literals
- * Literals are native frunctions that are generally defined in their simplistic literla
- * form in the source.
+/**
+ * Represents a container that stores and creates all instances of literals
+ * Literals are native frunctions that are generally defined in their simplistic
+ * literla form in the source.
  * 
- * Some types are:
- *  - boolean
- *  - integer
- *  - real
- *  - string
+ * Some types are: - boolean - integer - real - string
  * 
- * This class shall be a singleton and furthermore shall be called for all literal parsing.
- */ 
+ * This class shall be a singleton and furthermore shall be called for all
+ * literal parsing.
+ */
 public class LiteralDictionary implements Container
 {
   public static final boolean DEBUG = false;
-  
+
   private static LiteralDictionary instance = null;
-  /**Will create a new dictionary if one doesn't exist, otherwise it 
-   * return the current singleton that does exists.
+
+  /**
+   * Will create a new dictionary if one doesn't exist, otherwise it return the
+   * current singleton that does exists.
    * 
    * @return
    */
@@ -41,60 +41,91 @@ public class LiteralDictionary implements Container
   }
 
   // This will store all the literals for quick lookup
-  //   This is also what makes all literals the same objects.
+  // This is also what makes all literals the same objects.
   private HashMap<String, SymbolBinding> stored_literals = new HashMap<>();
-  
-  @Override public MethodBinding getMethod(String[] types, Frunction[] inputs){return null;}
 
-  @Override public void addMethod(MethodBinding binding){}
-  
-  @Override public List<MethodBinding> getMethods(){return null;}
+  @Override public MethodBinding getMethod(String[] types, Frunction[] inputs)
+  {
+    return null;
+  }
+
+  @Override public void addMethod(MethodBinding binding)
+  {}
+
+  @Override public List<MethodBinding> getMethods()
+  {
+    return null;
+  }
 
   @Override public SymbolBinding getSymbol(String symbol)
   {
-  	// Returning it if it already exists
-    if(this.stored_literals.containsKey(symbol)) return this.stored_literals.get(symbol);
-    
+    // Returning it if it already exists
+    if(this.stored_literals.containsKey(symbol))
+      return this.stored_literals.get(symbol);
+
     Frunction f = NativeFrunction.parseLiteral(symbol, this);
     if(f == null) return null;
-    
-    if(DEBUG) System.out.println("  Created literal binding for '" + symbol + "' with type '"+ f.getType() + "'");
-    
+
+    if(DEBUG)
+      System.out.println("  Created literal binding for '" + symbol
+          + "' with type '" + f.getType() + "'");
+
     // Creating the binding and adding it to the map
     SymbolBinding b = new DynamicSymbolBinding(symbol, f, this);
     this.stored_literals.put(symbol, b);
-    
+
     // Returning the new binding.
     return b;
   }
 
-  @Override public void addSymbol(SymbolBinding binding){}
+  @Override public void addSymbol(SymbolBinding binding)
+  {}
 
-  @Override public List<SymbolBinding> getSymbols(){return null;}
-  
-  @Override public Container getEnvironment(){return null;}
-  
-  /**Will return true if the symbol can be depicted as a literal.
+  @Override public List<SymbolBinding> getSymbols()
+  {
+    return null;
+  }
+
+  @Override public Container getEnvironment()
+  {
+    return null;
+  }
+
+  /**
+   * Will return true if the symbol can be depicted as a literal.
    * 
-   * @param symbol The symbol to check for.
+   * @param symbol
+   *          The symbol to check for.
    * @return True if the symbol represents a literal
    */
   public boolean isLiteral(String symbol)
   {
     symbol = symbol.trim();
-    if(symbol.startsWith(StringFrunction.STRING_CAPSULE) && symbol.endsWith(StringFrunction.STRING_CAPSULE)) return true;
-    if(symbol.equals(BooleanFrunction.TRUE_LITERAL) || symbol.equals(BooleanFrunction.FALSE_LITERAL)) return true;
-    
+    if(symbol.startsWith(StringFrunction.STRING_CAPSULE)
+        && symbol.endsWith(StringFrunction.STRING_CAPSULE)) return true;
+    if(symbol.equals(BooleanFrunction.TRUE_LITERAL)
+        || symbol.equals(BooleanFrunction.FALSE_LITERAL)) return true;
+
     // testing for double
     Double double_test = null;
-    try{double_test = Double.parseDouble(symbol);}catch(NumberFormatException e){}
+    try
+    {
+      double_test = Double.parseDouble(symbol);
+    }
+    catch(NumberFormatException e)
+    {}
     if(double_test != null) return true;
-    
+
     // testing for integer
     Integer integer_test = null;
-    try{integer_test = Integer.parseInt(symbol);}catch(NumberFormatException e){}
+    try
+    {
+      integer_test = Integer.parseInt(symbol);
+    }
+    catch(NumberFormatException e)
+    {}
     if(integer_test != null) return true;
-    
+
     return false;
   }
 
