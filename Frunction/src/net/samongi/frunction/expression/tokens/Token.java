8285b8d87f200fc3213678d660623d144fb1ffdc
Migsect <alsamon@yahoo.com>
2016-04-08 22:59:52 -0400

    missed file

--- Frunction/src/net/samongi/frunction/expression/tokens/Token.java
+++ Frunction/src/net/samongi/frunction/expression/tokens/Token.java
@@ -1,45 +1,42 @@
 package net.samongi.frunction.expression.tokens;
 
-import net.samongi.frunction.expression.exceptions.TokenException;
+import net.samongi.frunction.exceptions.parsing.TokenException;
+import net.samongi.frunction.frunction.literal.StringFrunction;
 
 public interface Token
 {
-  /**
-   * Types of the tokens
-   */
+  /** Types of the tokens */
   public enum Type
   {
-    SYMBOL, ACCESSOR, GROUP, INPUT, FRUNCTION
+    SYMBOL,
+    ACCESSOR,
+    GROUP,
+    INPUT,
+    FRUNCTION;
   }
 
-  /**
-   * Gets the scope openers
+  /** Gets the scope openers
    * 
-   * @return A list of strings that identify scope openers
-   */
+   * @return A list of strings that identify scope openers */
   public static String[] getScopeOpenIdentifiers()
   {
     return new String[] { FrunctionToken.OPEN, InputToken.OPEN };
   }
 
-  /**
-   * Gets the scope closers
+  /** Gets the scope closers
    * 
-   * @return A list of strings that identify scope closers
-   */
+   * @return A list of strings that identify scope closers */
   public static String[] getScopeCloseIdentifiers()
   {
     return new String[] { FrunctionToken.CLOSE, InputToken.CLOSE };
   }
 
-  /**
-   * Gets the scope toggles
+  /** Gets the scope toggles
    * 
-   * @return
-   */
+   * @return */
   public static String[] getScopeeToggleIdentifiers()
   {
-    return new String[] { "\"" };
+    return new String[] { StringFrunction.STRING_CAPSULE };
   }
 
   public static Token.Type[] arrayToTypeArray(Token[] tokens)
@@ -50,33 +47,24 @@ public interface Token
     return type_array;
   }
 
-  /**
-   * Parses the string trying to find tokens If multiple tokens are found, it
-   * will return a group token.
+  /** Parses the string trying to find tokens If multiple tokens are found, it will return a group token.
    * 
    * @param source
    * @return
-   * @throws TokenException
-   */
+   * @throws TokenException */
   public static GroupToken parseTokens(String source) throws TokenException
   {
     return new GroupToken(source);
   }
 
-  /**
-   * Returns the source of this token The source will be a string that the token
-   * is generated from. This will not include identifiers for the token such as
-   * parens for input tokens.
+  /** Returns the source of this token The source will be a string that the token is generated from. This will not
+   * include identifiers for the token such as parens for input tokens.
    * 
-   * @return
-   */
+   * @return */
   public String getSource();
 
-  /**
-   * Returns the type of the token. This can be used with just the class but
-   * this is more official
+  /** Returns the type of the token. This can be used with just the class but this is more official
    * 
-   * @return
-   */
+   * @return */
   public Token.Type getType();
 }
